2e896d2d815ad5e057d32ac1b632fc2abcf56315
AyushRatan <ayushratan.974@gmail.com>
2023-05-08 15:38:03 +0530

    uploaded files

---
+++ main.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+def login(username,password):
+    return {"username":username,"password":password}
+
+with gr.Blocks() as demo:
+    username = gr.Textbox(label="username")
+    password = gr.Textbox(label="password")
+    output = gr.Textbox(label="Output Box")
+    greet_btn = gr.Button("Greet")
+    greet_btn.click(fn=login, inputs=[username,password], outputs=output, api_name="greet")
+
+
+demo.launch()
